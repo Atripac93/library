0a150f5d1dfe0ec154325eaa1e0f7f14b3c461a9
Carlo Atripaldi <carloatrica@gmail.com>
2024-02-14 16:14:46 +0100

    Create App.js

---
+++ library/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "./Nav/Navigation";
+import Main from "./Main/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
