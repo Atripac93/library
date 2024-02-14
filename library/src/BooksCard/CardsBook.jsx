2f93aa5db4e6869f4ef6955e49c895166bd02d2f
Carlo Atripaldi <carloatrica@gmail.com>
2024-02-14 17:41:31 +0100

    Update CardsBook.jsx

--- library/src/BooksCard/CardsBook.jsx
+++ library/src/BooksCard/CardsBook.jsx
@@ -3,19 +3,15 @@ import Card from "react-bootstrap/Card";
 
 const CardsBooks = ({ title, category, img, price }) => {
   return (
-    <div className="row">
-      <div className="xs=12 md=6 lg=3">
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={img} />
-          <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>{category}</Card.Text>
-            <Card.Text>{price}</Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </div>
-    </div>
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={img} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{category}</Card.Text>
+        <Card.Text>{price}</Card.Text>
+        <Button variant="primary">Vai ai dettagli del libro</Button>
+      </Card.Body>
+    </Card>
   );
 };
 export default CardsBooks;
