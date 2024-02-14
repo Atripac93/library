e6b6f34e05a4f15e77caa4d13354d9c85147e47d
Carlo Atripaldi <carloatrica@gmail.com>
2024-02-14 17:41:28 +0100

    Update App.js

--- library/src/App.js
+++ library/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navigation from "./Nav/Navigation";
 import Main from "./Main/Main";
+import EndPage from "./Footer/EndPage";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Main />
+      <EndPage />
     </div>
   );
 }
